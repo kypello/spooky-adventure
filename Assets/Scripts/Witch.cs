3b4728a4c45040b44e724839dd78047f3524b29c
kypello <ollyjcrowe@gmail.com>
2021-10-17 19:32:58 +0100

    Started work on witch potions

--- Assets/Scripts/Witch.cs
+++ Assets/Scripts/Witch.cs
@@ -4,9 +4,44 @@ using UnityEngine;
 
 public class Witch : Interactable
 {
+    public Animation itemDrop;
+
+    public GameObject[] itemModels;
+    public Color[] colors;
+    public Color[] altColors;
+
+    public ParticleSystem[] particles;
+    public ParticleSystem burst;
+
+    public Material potionMat;
+
     public override IEnumerator Interact() {
         DisablePlayer();
         yield return textBubble.Display("Witch", "I am a witch hehehehehe");
+        yield return DropItemInPotion(0);
         EnablePlayer();
+    }
+
+    IEnumerator DropItemInPotion(int item) {
+        foreach (GameObject itemModel in itemModels) {
+            itemModel.SetActive(false);
+        }
+
+        itemModels[item].SetActive(true);
+
+        itemDrop.Play();
+        yield return new WaitForSeconds(0.75f);
+
+        ParticleSystem.MinMaxGradient minMaxGradient = new ParticleSystem.MinMaxGradient(colors[item], altColors[item]);
+
+        foreach (ParticleSystem particleSystem in particles) {
+            ParticleSystem.MainModule main = particleSystem.main;
+            main.startColor = minMaxGradient;
+        }
+
+        burst.Play();
+
+        potionMat.SetColor("_Background", colors[item]);
+        potionMat.SetColor("_Swirl", colors[item]);
     }
 }
