929cd533510f1c04ae964105f0116028564476ca
kypello <ollyjcrowe@gmail.com>
2021-10-28 22:48:37 +0100

    Added a bit of characters

--- Assets/Scripts/SimplePatroller.cs
+++ Assets/Scripts/SimplePatroller.cs
@@ -5,11 +5,31 @@ using UnityEngine;
 public class SimplePatroller : Patroller
 {
     public string name;
-    public string[] lines;
+
+    int interactions = 0;
+
+    [System.Serializable]
+    public struct LinesArray {
+        [TextArea]
+        public string[] lines;
+    }
+
+    public LinesArray[] lines;
+
+    public Vector3 lookOffset;
 
     public override IEnumerator StopAndChat() {
-        foreach (string line in lines) {
+        DisablePlayer();
+        StartCoroutine(playerLook.LookAt(transform.position + lookOffset));
+
+        foreach (string line in lines[interactions].lines) {
             yield return textBubble.Display(name, line);
         }
+
+        if (interactions < lines.Length - 1) {
+            interactions++;
+        }
+
+        EnablePlayer();
     }
 }
